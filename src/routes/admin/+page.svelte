cc103c45a3444850cf9f4661bd25637c703e3da9
Louis T <67950802+LouisT2469@users.noreply.github.com>
2024-05-06 21:31:50 +0200

    chore: Refactor layout and configuration files, update PocketBase URL in +page.svelte

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,4 +1,22 @@
-<script lang="ts"></script>
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import PocketBase from 'pocketbase';
+
+	const pb = new PocketBase('https://chat-ms.app.louist2469.de');
+
+	async function deleteChat() {
+		const records = await pb.collection('message').getFullList({
+			sort: '-created'
+		});
+
+		records.forEach((records) => {
+			//console.log('ID:', records.id);
+			pb.collection('message').delete(records.id);
+		});
+	}
+
+	let visibleDel = false; //false
+</script>
 
 <div class="flex h-screen w-full flex-col">
 	<div class="sticky top-0 z-50">
@@ -6,6 +24,9 @@
 			<span class="text-lg font-semibold text-white">Admin-Pannel</span>
 			<div class="flex items-baseline gap-x-1">
 				<button
+					on:click={() => {
+						goto('/');
+					}}
 					class="rounded-md bg-white px-2 py-1 font-bold text-black hover:scale-[1.02] focus:bg-zinc-200"
 					>Chat-Site</button
 				>
@@ -16,12 +37,29 @@
 		<div class="flex h-fit w-fit flex-col rounded-md bg-[#3b3b3b] px-4 py-2">
 			<span class="text-lg font-bold text-gray-200">Chat-Actions:</span>
 			<span class="mb-1 mt-2 italic text-gray-300">Export the complete chat.</span>
-			<button class=" rounded-md bg-zinc-900/90 px-2 py-1 font-bold text-zinc-300/90"
-				>Export</button
+			<button
+				class=" rounded-md bg-zinc-900/90 px-2 py-1 font-bold text-zinc-300/90"
+				on:click={() => {
+					goto('/admin/export');
+				}}>Export</button
 			>
 
-			<span class="mb-1 mt-6 italic text-gray-300">Submit, to delete this entire chat!</span>
-			<button class=" rounded-md bg-red-900/90 px-2 py-1 font-bold text-zinc-300/90">Submit</button>
+			<span class="mb-1 mt-6 italic text-gray-300">Delete the entire chat!</span>
+			<button
+				class=" rounded-md bg-red-900/90 px-2 py-1 font-bold text-zinc-300/90"
+				on:click={() => {
+					deleteChat();
+					visibleDel = true;
+					setTimeout(() => {
+						visibleDel = false;
+					}, 1000);
+				}}
+				>{#if visibleDel}
+					<span>Successfully deleted!</span>
+				{:else}
+					Delete
+				{/if}</button
+			>
 		</div>
 	</div>
 	<div class="static">
--- src/routes/admin/export/+page.svelte
+++ src/routes/admin/export/+page.svelte
@@ -1,23 +1,13 @@
 <script lang="ts">
 	import PocketBase from 'pocketbase';
-	import { onMount, afterUpdate } from 'svelte';
-	import { onDestroy } from 'svelte';
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
 
 	const pb = new PocketBase('https://chat-ms.app.louist2469.de');
 
 	let user: string | null = null;
 	let messages: any[] = [];
 	let send = '';
-
-	// Element-Referenz für den Nachrichten-Container
-	let messagesContainer: HTMLDivElement | null = null;
-
-	// Funktion zum Scrollen nach unten
-	const scrollToBottom = () => {
-		if (messagesContainer) {
-			messagesContainer.scrollTop = messagesContainer.scrollHeight;
-		}
-	};
 
 	onMount(async () => {
 		const storedUsername = localStorage.getItem('username');
@@ -37,44 +27,25 @@
 			const records = await pb.collection('message').getFullList({
 				sort: '-created'
 			});
-			console.log(records);
 			messages = records.reverse();
-			pb.collection('message').subscribe('*', (e) => {
-				if (e.record.user !== user) {
-					messages.push(e.record);
-					messages = [...messages]; // Force re-render
-					scrollToBottom(); // Scroll beim Hinzufügen einer Nachricht
-				}
-			});
-
-			scrollToBottom(); // Scroll beim ersten Laden
 		} catch (error) {
 			console.error(error);
 		}
-	});
 
-	afterUpdate(() => {
-		scrollToBottom(); // Scroll bei Änderungen in der Nachrichtenliste
+		setTimeout(() => {
+			window.print();
+		}, 1000);
+		setTimeout(() => {
+			goto('/admin');
+		}, 2000);
 	});
-
-	async function sendMessage(user: string, message: string) {
-		try {
-			pb.autoCancellation(false);
-			const record = await pb.collection('message').create({
-				user,
-				message
-			});
-			messages = [...messages, record];
-		} catch (error) {
-			console.error(error);
-		}
-	}
 </script>
 
 <div class="flex h-screen w-full flex-col">
 	<div class="sticky top-0 z-50">
 		<div class="flex w-full items-center justify-between rounded-b-lg bg-black px-4 py-4">
 			<span class="text-lg font-semibold text-white">Chat-Medienscouts</span>
+			<span class="text-lg font-semibold text-white">Workshop - Cybermobbing</span>
 			<div class="flex items-baseline gap-x-1">
 				<span class="font-semibold text-white">Name:</span>
 				<span class="text-white">{user}</span>
@@ -82,7 +53,7 @@
 		</div>
 	</div>
 
-	<div class="mb-2 mt-2 h-full w-full overflow-y-scroll" bind:this={messagesContainer}>
+	<div class="mb-2 mt-2 h-full w-full">
 		<div class="flex h-full w-full flex-col items-center gap-y-2">
 			{#each messages as message}
 				{#if message.user === 'System' || message.user === 'system'}
@@ -104,52 +75,10 @@
 			{/each}
 		</div>
 	</div>
-
-	<div class="static">
-		<form
-			on:submit|preventDefault={async () => {
-				await sendMessage(user, send);
-				send = '';
-			}}
-			class="flex w-full items-center justify-center gap-x-4 rounded-t-lg bg-black p-3"
-		>
-			<input
-				required
-				class="w-11/12 rounded-md px-2 py-1 outline-none"
-				placeholder="Gib eine Nachricht ein."
-				type="text"
-				bind:value={send}
-			/>
-			<button type="submit" class="material-icons text-3xl text-white">send</button>
-		</form>
-	</div>
 </div>
 
 <style>
 	:root {
 		background-color: #171717;
-		--sb-track-color: #171717;
-		--sb-thumb-color: #000000;
-		--sb-size: 14px;
-	}
-
-	::-webkit-scrollbar {
-		width: var(--sb-size);
-	}
-
-	::-webkit-scrollbar-track {
-		background: var(--sb-track-color);
-		border-radius: 10px;
-	}
-
-	::-webkit-scrollbar-thumb {
-		background: var(--sb-thumb-color);
-		border-radius: 10px;
-	}
-
-	@supports not selector(::-webkit-scrollbar) {
-		:root {
-			scrollbar-color: var(--sb-thumb-color) var(--sb-track-color);
-		}
 	}
 </style>
